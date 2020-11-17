bc67f9e3a19a57f1822a31f0ea4a5d7f1c0b3133
Alexander <kava3331@mail.ru>
2020-11-18 00:49:12 +0300

    Used eslint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import './App.css';
-import Logo from './components/Logo/Logo.js';
+import "./App.css";
+import Logo from './components/Logo/Logo';
 import Tickets from './components/Tickets/Tickets';
 
-
 class App extends React.Component {
-
- 
-
-
-  render () {
-    
+  render() {
     return (
       <div className="App">
-        <Logo/>
-        <Tickets/>
+        <Logo />
+        <Tickets />
       </div>
     );
-
-  };
-  
+  }
 }
 
 export default App;
