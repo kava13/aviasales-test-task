9cd685e4ace76ff83cfee7489c0f26a14372960f
Alexander <kava3331@mail.ru>
2020-11-17 12:39:22 +0300

    Changed profitableFilters component

--- src/components/Tickets/ProfitableFilters.js
+++ src/components/Tickets/ProfitableFilters.js
@@ -4,6 +4,16 @@ import axios from 'axios';
 import '../../css/profitableFilters.css';
 
 import * as actions from '../../actions/index.js';
+import cn from 'classnames'
+
+import { getProfitableFilterName } from '../../selectors/index.js';
+
+const mapStateToProps = (state) => {
+  const props = {
+      profitableFilterName: getProfitableFilterName(state)
+  }
+  return props;
+}
 
 const actionCreators = {
     setTicketsProfitableFilter: actions.setTicketsProfitableFilter,
@@ -12,16 +22,30 @@ const actionCreators = {
 class ProfitableFilters extends React.Component {
 
     handleSetTicketsProfitableFilter = (filterName) => () => {
-        const { setTicketsProfitableFilter } = this.props;
+        const { setTicketsProfitableFilter, profitableFilterName } = this.props;
+        filterName = filterName === profitableFilterName? 'all' : filterName;
         setTicketsProfitableFilter({ filterName });
     }
 
   render () {
+
+    const { profitableFilterName } = this.props;
   
+    const buttonLeftStyle = cn('tickets-button', {
+      'tickets-button__left': true,
+      'tickets-button__active': profitableFilterName === 'cheapest'
+
+    });
+
+    const buttonRightStyle = cn('tickets-button', {
+      'tickets-right': true,
+      'tickets-button__active': profitableFilterName === 'fastest'
+    })
+
     return (
       <div className="tickets-buttons">
-          <button className="tickets-button" onClick={this.handleSetTicketsProfitableFilter('cheapest')}>ДЕШЕВЫЕ</button>
-          <button className="tickets-button" onClick={this.handleSetTicketsProfitableFilter('fastest')}>БЫСТРЫЕ</button>
+          <button className={buttonLeftStyle} onClick={this.handleSetTicketsProfitableFilter('cheapest')}>САМЫЕ ДЕШЕВЫЕ</button>
+          <button className={buttonRightStyle} onClick={this.handleSetTicketsProfitableFilter('fastest')}>САМЫЕ БЫСТРЫЕ</button>
       </div>
     );
 
@@ -29,4 +53,4 @@ class ProfitableFilters extends React.Component {
   
 }
 
-export default connect(null, actionCreators)(ProfitableFilters);
+export default connect(mapStateToProps, actionCreators)(ProfitableFilters);
